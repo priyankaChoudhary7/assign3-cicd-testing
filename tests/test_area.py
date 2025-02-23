586f2f41ab801f066f996d0b1152d523e0635f69
priyankaChoudhary7 <choudharypriyanka178@gmail.com>
2025-02-23 14:50:42 -0500

    created yaml file

--- tests/test_area.py
+++ tests/test_area.py
@@ -12,3 +12,8 @@ def test_calculate_area_square_string():
 def test_calculate_area_square_list():  
     with pytest.raises(TypeError):  
         calculate_area_square([2])
+
+#using last two digits of my student number
+def test_area_student_id():
+    with pytest.raises(TypeError):  
+        calculate_area_square("52")
